70765200d6e8c1038246d0e435e290239fc39dd3
HvLam <rockiret02@gmail.com>
2020-11-06 20:54:59 +0700

    new origin

--- demo.js
+++ demo.js
@@ -1,6 +1,9 @@
 console.log("Khởi tạo dự án");
 // B1: git init
 // B2: git add .
-// B3: git commit -m ""
-// B4: git push
+// B3: git commit -m "" :tạo 1 file ảo để lưu code
+// B4: git push đẩy commit lên
+//  git clone : lấy link cho dự án mới
+// B5: git pull
 console.log("code of Son");
+console.log("code new branch");
